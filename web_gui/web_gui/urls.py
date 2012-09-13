e32b1048c7d5679d951e2e22848a4a6a0287ece6
Kuba Sejdak <kuba.sejdak@gmail.com>
2012-09-13 17:41:18 +0200

    web_gui is working on both development and release server

--- web_gui/web_gui/urls.py
+++ web_gui/web_gui/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, include, url
-from graph_analyzer.views import show_home, show_analyze, show_browse,\
-	show_options
+from graph_analyzer.views import show_home, show_analyze, show_browse, show_options
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin

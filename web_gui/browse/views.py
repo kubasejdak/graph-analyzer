db1bb322b9ddfa7f82d652219d49f3b38e2c1c45
kuba.sejdak@gmail.com <kuba.sejdak@gmail.com>
2012-12-08 22:39:32 +0100

    loops now have working hashes

--- web_gui/browse/views.py
+++ web_gui/browse/views.py
@@ -7,7 +7,7 @@ import os.path
 from stat import S_IRWXG, S_IRWXO, S_IRWXU
 
 from options.models import SystemInfo
-from analyze.models import Sample, SampleGroup, GroupAssignment, LoopAssignment, APIAssignment, HashAssignment
+from analyze.models import Sample, SampleGroup, GroupAssignment, LoopAssignment, Loop, APIAssignment, HashAssignment
 
 def show_browse(request):
     # get system info (only one object should exists)
@@ -67,6 +67,38 @@ def show_browse(request):
     # ===================================== POST =====================================
 
     if request.method == "POST":
+        if "save_loop" in request.POST:
+            # save comment
+            c.update({"is_loopmessage": True})
+            tmp_loopassignment = LoopAssignment.objects.get(id = request.POST["save_loop"])
+            new_loop = Loop.objects.get(id = tmp_loopassignment.loop.id)
+            new_loop.comment = request.POST["loop_comments"]
+            new_loop.save()
+            
+            # get sample info and reload page
+            show_sample = Sample.objects.get(id = tmp_loopassignment.sample.id)
+            show_loopassignment = LoopAssignment.objects.filter(sample = tmp_loopassignment.sample.id)
+            show_apiassignment = APIAssignment.objects.filter(sample = tmp_loopassignment.sample.id)
+            show_hashassignment = HashAssignment.objects.get(sample = tmp_loopassignment.sample.id)
+            show_groupassignment = GroupAssignment.objects.filter(member = tmp_loopassignment.sample.id).order_by("-resemblence_rate")
+
+            # get graph picture
+            graph_source = show_sample.graph_name
+            graph_file = graph_source.rpartition("/")[2]
+            graph_dir = STATICFILES_DIRS[0] + "tmp_graphs/"
+            if not os.path.exists(graph_dir + graph_file):
+                shutil.copy(graph_source, STATICFILES_DIRS[0] + "tmp_graphs")
+                os.chmod(graph_dir + graph_file, S_IRWXU | S_IRWXG | S_IRWXO)
+
+            c.update({"show_sample": show_sample,
+					  "show_loopassignment": show_loopassignment,
+					  "show_apiassignment": show_apiassignment,
+					  "show_hashassignment": show_hashassignment,
+					  "show_groupassignment": show_groupassignment,
+					  "graph_file": graph_file,
+					  "loops_tab": "active"})
+            return render_to_response("show.html", c)
+
         if "comments" in request.POST:
             c.update({"is_message": True})
             new_sample = Sample.objects.get(id = request.POST["show_sample"])
@@ -94,7 +126,8 @@ def show_browse(request):
 					  "show_apiassignment": show_apiassignment,
 					  "show_hashassignment": show_hashassignment,
 					  "show_groupassignment": show_groupassignment,
-					  "graph_file": graph_file})
+					  "graph_file": graph_file,
+					  "general_tab": "active"})
             return render_to_response("show.html", c)
         
         if "show_group" in request.POST:
@@ -126,6 +159,38 @@ def show_browse_groups(request):
     # ===================================== POST =====================================
 
     if request.method == "POST":
+        if "save_loop" in request.POST:
+            # save comment
+            c.update({"is_loopmessage": True, "graph_tab": "active"})
+            tmp_loopassignment = LoopAssignment.objects.get(id = request.POST["save_loop"])
+            new_loop = Loop.objects.get(id = tmp_loopassignment.loop.id)
+            new_loop.comment = request.POST["loop_comments"]
+            new_loop.save()
+            
+            # get sample info and reload page
+            show_sample = Sample.objects.get(id = tmp_loopassignment.sample.id)
+            show_loopassignment = LoopAssignment.objects.filter(sample = tmp_loopassignment.sample.id)
+            show_apiassignment = APIAssignment.objects.filter(sample = tmp_loopassignment.sample.id)
+            show_hashassignment = HashAssignment.objects.get(sample = tmp_loopassignment.sample.id)
+            show_groupassignment = GroupAssignment.objects.filter(member = tmp_loopassignment.sample.id).order_by("-resemblence_rate")
+
+            # get graph picture
+            graph_source = show_sample.graph_name
+            graph_file = graph_source.rpartition("/")[2]
+            graph_dir = STATICFILES_DIRS[0] + "tmp_graphs/"
+            if not os.path.exists(graph_dir + graph_file):
+                shutil.copy(graph_source, STATICFILES_DIRS[0] + "tmp_graphs")
+                os.chmod(graph_dir + graph_file, S_IRWXU | S_IRWXG | S_IRWXO)
+
+            c.update({"show_sample": show_sample,
+					  "show_loopassignment": show_loopassignment,
+					  "show_apiassignment": show_apiassignment,
+					  "show_hashassignment": show_hashassignment,
+					  "show_groupassignment": show_groupassignment,
+					  "graph_file": graph_file,
+					  "loops_tab": "active"})
+            return render_to_response("show.html", c)
+
         if "comments" in request.POST:
             c.update({"is_message": True})
             new_group = SampleGroup.objects.get(id = request.POST["show_group"])
@@ -161,5 +226,6 @@ def show_browse_groups(request):
 					  "show_apiassignment": show_apiassignment,
 					  "show_hashassignment": show_hashassignment,
 					  "show_groupassignment": show_groupassignment,
-					  "graph_file": graph_file})
+					  "graph_file": graph_file,
+					  "general_tab": "active"})
             return render_to_response("show.html", c)

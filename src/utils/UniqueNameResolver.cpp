a5093cb286e64e0c05117e311d6af0409f69dba1
Kuba Sejdak <kuba.sejdak@gmail.com>
2013-07-24 22:48:29 +0200

    Movied UniqueNameResolver implementation from Qt to boost::filesystem.

--- src/utils/UniqueNameResolver.cpp
+++ src/utils/UniqueNameResolver.cpp
@@ -8,33 +8,33 @@
 
 #include <string>
 #include <sstream>
-#include <QFile>
-#include <QFileInfo>
+#include <boost/filesystem.hpp>
 
 #include <utils/SystemLogger.h>
-#include <utils/Toolbox.h>
 
 using namespace std;
 
 string UniqueNameResolver::resolve(string filename)
 {
-	string uniqueName = filename;
+	boost::filesystem::path original = boost::filesystem::absolute(filename);
+
+	string uniqueName = original.generic_string();
+
+	boost::filesystem::path extension = original.extension();
+	boost::filesystem::path stem = original.stem();
+	boost::filesystem::path directory = original.remove_filename();
+
+	boost::filesystem::path absolutePath = uniqueName;
+
 	int k = 2;
-	while(QFile(uniqueName.c_str()).exists()) {
+	while(boost::filesystem::exists(absolutePath) == true) {
 		LOG("duplicate for given filename found, renaming\n");
-		QFileInfo duplicate(uniqueName.c_str());
+
 		stringstream ss;
-		ss << duplicate.absolutePath().toStdString() << "/" << duplicate.baseName().toStdString();
+		ss << directory.generic_string() << "/" << stem.generic_string() << "_" << k << extension.generic_string();
 		uniqueName = ss.str();
+		absolutePath = uniqueName;
 
-		if(k != 2) {
-			int n = uniqueName.find_last_of('_');
-			uniqueName = uniqueName.substr(0, n + 1);
-		}
-		else
-			uniqueName += "_";
-
-		uniqueName += Toolbox::itos(k) + ".png";
 		LOG("renamed to: [%s]\n", uniqueName.c_str());
 		++k;
 	}
